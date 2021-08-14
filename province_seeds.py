a54566f4d79c15b0c6979f2c15c44759b2dd9295
havodaxe <havodaxe@gmail.com>
2021-08-14 17:54:45 +0200

    Add "land-coloured" seeding
    Red might be a stretch but we take all the colours we can get that
    don't contain much of blue.

--- province_seeds.py
+++ province_seeds.py
@@ -12,21 +12,26 @@ def generate_seeds(land=True):
     input for the province generator.
     """
     INITRED = 0
-    INITGREEN = 85
-    INITBLUE = 170
-    # By keeping the colours offset like this we ensure that the first few
-    # colours at least are saturated and not complete black
+    INITGREEN = 0
+    INITBLUE = 0
     seed(0)
     seed_coords = []
 
     out = Image.new("RGBA", (5632,2048), (0,0,0,255))
 
+    redmodulus = 192
+    redminimum = 48
+    greenmodulus = 192
+    greenminimum = 48
+    bluemodulus = 32
+    blueminimum = 0
+
     for i in range(1600):
         seed_coords.append(( randint(0, out.width - 1),
                              randint(0, out.height - 1) ))
-        red =   (INITRED   + 13 * i + i // 256)   % 256
-        green = (INITGREEN + 17 * i + i // 65536) % 256
-        blue =  (INITBLUE  + 19 * i)              % 256
+        red =   (INITRED   + 13 * i + i // 256)   % redmodulus + redminimum
+        green = (INITGREEN + 17 * i + i // 65536) % greenmodulus + greenminimum
+        blue =  (INITBLUE  + 19 * i)              % bluemodulus + blueminimum
         # After each loop through the 256 colour values, red shifts over by 1
         # And after 256 * 256 loops, green shifts over by 1
         # This process ensures that eventually, each colour gets visited.
